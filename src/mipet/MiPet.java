5273148ee19098dd4923a7915ce73de842469d1b
37h4n1821 <ethant.leiva@gmail.com>
2023-11-14 12:24:07 -0300

    Update MiPet.java

--- src/mipet/MiPet.java
+++ src/mipet/MiPet.java
@@ -16,6 +16,7 @@ public class MiPet {
     public static void main(String[] args) {
         // TODO code application logic here
         Tipo_Mascota tipo=new Tipo_Mascota();
+        //chupalo
     }
     
 }
